f341792bd6882e978c3e4a524a833c72ac17550a
nio74 <mpaolo74@gmail.com>
2020-11-21 06:37:15 +0100

    for question

---
+++ include/ClassShader.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+#include "ClassShader.h"
+
+
+
+ClassShader::ClassShader(std::string string)
+{
+    std::cout << "this ìs the test  " <<string << std::endl;
+}
+
+
+ClassShader::~ClassShader()
+{
+}
+
---
+++ include/ClassShader.h
@@ -0,0 +1,11 @@
+#include <iostream>
+class ClassShader
+{
+private:
+    /* data */
+public:
+    ClassShader(std::string string);
+    ~ClassShader();
+};
+
+
